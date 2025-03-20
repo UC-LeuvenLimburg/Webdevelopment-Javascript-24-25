df3f800efc6c445929f7d59cdd13a0c5d28da84a
Mimi Willems <u0067591@ucll.be>
2025-03-20 13:02:41 +0100

    add HERE Maps Geocoding markers tonen zonder infobubble

---
+++ javascript/HEREMapsGeocoding/campussenLijst.js
@@ -0,0 +1,48 @@
+var ucllCampi = [
+	{
+		naam:"CAMPUS DIEPENBEEK",
+		info:"De grootste campus van UCLL in het groen. Campus Diepenbeek is de grootste campus van onze hogeschool. In totaal studeren er 4.000 studenten op deze campus.Op deze campus vind je de opleidingen van volgende programma's: Management, Technologie, Lerarenopleiding en  Gezondheid. De studenten industrieel ingenieur van de KU Leuven - UHasselt vertoeven ook vaak op onze campus. Ook vind je hier en ruime parking, goede bereikbaarheid met openbaar vervoer en alle Limburgse algemene diensten.",
+		adres:"Agoralaan, gebouw B bus 1, 3590 Diepenbeek"
+	},
+	{
+		naam:"CAMPUS DIEST",
+		info:"Onze campus in hartje Diest. Campus Diest is zeer gemakkelijk bereikbaar, want het is gelegen vlakbij een centraal busknooppunt en op slechts 400 meter van het treinstation van Diest. De campus ligt ook op een boogscheut van het centrum van Diest. Een sympathiek, charmant provinciestadje op mensenmaat tussen de glooiende heuvels van het Hageland. Gezellige straten en trendy winkels liggen om de hoek, en net buiten de kleine kern met stemmige cafeetjes en hippe restaurantjes zit je in het groen.",
+		adres:"Weerstandsplein 2, 3290 Diest"
+
+	},
+ 	{
+ 		naam:"CAMPUS GASTHUISBERG",
+ 		info:"Deze Leuvense campus is gelegen op site Gasthuisberg. De keuze voor deze locatie was een heel bewuste. Gasthuisberg staat in binnen- en buitenland bekend om zijn expertise op gebied van gezondheidszorg en technologie. Door onze aanwezigheid op deze site willen we de goede samenwerking met het UZ Leuven en de Associatie KU Leuven nog verder versterken. Via deze onderlinge kruisbestuiving kunnen wij de kwaliteit van ons professioneel onderwijs, waar UC Leuven-Limburg om bekend staat, voortdurend verbeteren. Op deze campus vind je de opleidingen Biomedische Laboratoriumtechnologie, Chemie, Mondzorg, Verpleegkunde, Voedings- en dieetkunde  en Vroedkunde,  samen met talloze banaba's en Postgraduaten.",
+ 		adres:"Herestraat 49, 3000 Leuven"
+ 	},
+	{
+		naam:"CAMPUS GENK",
+		info:"Een campus naast Ziekenhuis Oost-Limburg (ZOL), in het groen van Genk. Aan de rand van het nationaal park Hoge Kempen, midden in het groen, tegenover het Ziekenhuis Oost-Limburg (ZOL) in Genk beschikt UCLL over een moderne zorgcampus voor de opleidingen Verpleegkunde en Vroedkunde. De campus beschikt over een restaurant, mediatheek, aula's en heel wat skills labs.",
+		adres:"Synaps Park 5400 bus A, 3600 Genk"
+
+	},
+	{
+		naam:"CAMPUS HASSELT",
+		info:"Onze campus in hartje Hasselt. Achter het cultuurcentrum van Hasselt en het stadspark bevindt zich Campus Hasselt.  Deze campus is de thuisbasis van de graduaten Welzijn (maatschappelijk werk / orthopedagogische begeleiding / sociaal-cultureel werk) en de bachelor Orthopedagogie. De campus is charmant, gezellig en heeft een eigen bibliotheek en sportzaal. Er zijn tal van werk -en ontspanningszones voor de studenten. Buiten is er veel ruimte voor evenementen en met het stadspark vlakbij is er veel groen. De cafetaria heeft een ruim aanbod.",
+		adres:"Oude Luikerbaan 79, 3500 Hasselt"
+
+	},
+ 	{
+ 		naam:"CAMPUS HERTOGSTRAAT",
+ 		info:"Een campus in het groen op de rand van Leuven. Op onze campus Hertogstraat in Heverlee kan je de Lerarenopleiding Kleuter, Lager en Secundair Onderwijs en de Educatieve Graduaatsopleiding vinden. Ook de Banaba's Buitengewoon Onderwijs en Meertalig Onderwijs  en de postgraduaten van de Lerarenopleiding vinden plaats op deze campus. Vanaf academiejaar 2023-2024 is campus Hertogstraat ook de nieuwe thuisbasis van onze Leuvense graduaats-, banaba- & postgraduaats- en werken & leren- opleidingen van het interessegebied Economie, recht en ondernemen en het graduaat programmeren uit het interessegebied Informatica.",
+ 		adres:"Hertogstraat 178, 3001 Heverlee"
+
+ 	},
+ 	{
+ 		naam:"CAMPUS PROXIMUS",
+ 		info:"Op een boogscheut van Leuven tussen de bedrijven. UC Leuven-Limburg streeft ernaar haar campussen in de onmiddellijke nabijheid van het bedrijfsleven te situeren om samenwerking op het vlak van onderwijs, onderzoek en dienstverlening te stimuleren. Door zich te nestelen in de schoot van een bedrijventerrein, hoopt de hogeschool ook het levenslang leren meer kansen te geven (o.m. door samenwerking met VDAB en Syntra, beide buren van Campus Proximus).",
+ 		adres:"Geldenaaksebaan 335, 3001 Heverlee"
+
+ 	},
+ 	{
+ 		naam:"CAMPUS SOCIALE SCHOOL",
+ 		info:"Boven aan de Groeneweg in Heverlee, torent een hoog gebouw boven de omliggende velden uit. Het is campus Sociale School; op een boogscheut van bruisende studentenstad Leuven. Op deze groene en gezellige campus kan je je even uit de wereld terugtrekken om een 'overview' te krijgen van mens en samenleving.",
+ 		adres:"Groeneweg 151, 3001 Heverlee"
+
+ 	}
+];
